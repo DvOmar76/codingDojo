dbde743deb3664d8593b1e6957f2ce0ad79495a0
DvOmar76 <84674856+DvOmar76@users.noreply.github.com>
2021-10-06 00:52:46 +0300

    .

--- To_Do_App/app/src/main/java/com/example/to_do_app/MainActivity.kt
+++ To_Do_App/app/src/main/java/com/example/to_do_app/MainActivity.kt
@@ -7,5 +7,12 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var a = ""
+        for (i in 1..5) {
+            a += i
+        }
+        print(a)
+
     }
+
 }
