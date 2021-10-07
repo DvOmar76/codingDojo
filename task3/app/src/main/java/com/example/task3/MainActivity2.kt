d7c6aae75a50cbd87950abac83d6a4a5e7a87caa
DvOmar76 <84674856+DvOmar76@users.noreply.github.com>
2021-10-07 09:34:25 +0300

    task3

---
+++ task3/app/src/main/java/com/example/task3/MainActivity2.kt
@@ -0,0 +1,13 @@
+package com.example.task3
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main2.*
+
+class MainActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+        textView.setText("${intent.getStringExtra("name")}${intent.getStringExtra("location")}")
+    }
+}
